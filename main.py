cb3c35b09a4ac52cbf6f3aa018813ed3bd539425
Em Utdorm <utdorm@gmail.com>
2019-06-08 15:46:00 +0700

    Finding optimal combination of NN structures

--- main.py
+++ main.py
@@ -5,18 +5,9 @@ from keras.callbacks import TensorBoard
 import pickle
 import time
 
-getModelName = 'Plus-notPlus-1layer-1500node.{}'.format(int(time.time()))
-initTensorboard = TensorBoard(log_dir= './logs/{}'.format(getModelName)) 
-
-nodePerLayer = 1
-# nodePerLayer = 100
-# nodePerLayer = 1500
-
-
-# ep = 1
-# ep = 10
-# ep = 100
-# ep = 1500
+dense_layers = [1, 2, 3]
+layer_sizes = [1, 32, 64, 128]
+# 64-nodes_3-layers-1559982529 - Best result
 
 
 def main():
@@ -28,26 +19,40 @@ def main():
     y = pickle.load(pickle_in)
 
     X = X/255.0
+    
+    for dense_layer in dense_layers:
+        for layer_size in layer_sizes:
+            getModelName = "{}-nodes_{}-layers-{}".format(layer_size, dense_layer, int(time.time()))
 
-    model = Sequential()
+            model = Sequential()
 
-    model.add(Dense(nodePerLayer, activation='relu', input_shape = X.shape[1:]))
+            model.add(Dense(layer_size, activation='relu', input_shape=X.shape[1:]))
 
-    model.add(Flatten())
+            model.add(Flatten())
 
-    # model.add(Dense(64))
+            for _ in dense_layers:
 
-    model.add(Dense(1, activation='sigmoid'))
+                model.add(Dense(layer_size, activation='relu'))
 
-    model.compile(loss='binary_crossentropy',
-                    optimizer='adam', 
-                    metrics=['accuracy'])
+            model.add(Dense(1, activation='sigmoid'))
+            
+            initTensorboard = TensorBoard(log_dir="logs/{}".format(getModelName))
+            
+            model.compile(
+                            loss='binary_crossentropy',
+                            optimizer='adam',
+                            metrics=['accuracy']
+                        )
 
-    model.fit(X, y, epochs=10, 
-                batch_size=32, 
-                validation_split=0.1,
-                callbacks=[initTensorboard])
+            model.fit(
+                        X, y, 
+                        batch_size=32, 
+                        epochs=10, 
+                        validation_split=0.1, 
+                        callbacks=[initTensorboard]
+                    )
 
+            
 
 if __name__ == "__main__": 
 
